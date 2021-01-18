d7d6f47a220d8c9cf16831750ed1dee370d17dc1
andrejusnec <73877307+andrejusnec@users.noreply.github.com>
2021-01-18 13:26:32 +0200

    3 lygis
    3D masyvas  ir palyginimas su kitais masyvais

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -2,9 +2,13 @@ package gameoflife;
 
 public class GameOfLife {
 
+    final static int K = 10;
+
     public static void main(String[] args) {
-        char[][] field = new char[10][10];
-        char[][] field2 = new char[10][10];
+        char[][] field = new char[K][K];
+        char[][] field2 = new char[K][K];
+        char[][][] field3D = new char[50][K][K];
+        int cikliskumas = 0;
         /**
          * ***********************Fill the array**************************
          */
@@ -21,15 +25,23 @@ public class GameOfLife {
         int[] iV = new int[3];
         int[] jV = new int[3];
         boolean flagA, flagB; // flagA tai a lenteles ribos, flagB stulpeliu ribos
-        boolean arToksPats, arMire; 
+        boolean arToksPats, arMire;
         for (int i = 0; i < field.length; i++) {
-                for (int j = 0; j < field.length; j++) {
-                    System.out.print(field[i][j] + " ");
-                }
-                System.out.println();
+            for (int j = 0; j < field.length; j++) {
+                System.out.print(field[i][j] + " ");
             }
+            System.out.println();
+        }
         for (int abc = 0; abc < 50; abc++) {
-            arToksPats = true;
+            /**
+             * *****Uzpildau trimati masiva*****
+             */
+            for (int i = 0; i < field.length; i++) {
+                for (int j = 0; j < field.length; j++) {
+                    field3D[abc][i][j] = field[i][j];
+                }
+            }
+            arToksPats = false;
             arMire = true;
             System.out.println("");
             for (int a = 0; a < field.length; a++) {
@@ -83,22 +95,20 @@ public class GameOfLife {
                     }
                 }
             }
-            /**
-             * *************************Tikrinu ar sutampa esantis ir buves masyvai****************************/
-            for(int c = 0; c < field.length; c++) {
-                
-                for(int d = 0; d < field.length; d++) {
-                    if(field[c][d] != field2[c][d]) {
-                        arToksPats = false;
-                        break;
-                    }
-                }
-                if(!arToksPats) {
+
+            //arToksPats = arLygu(field, field2);
+            //Tikrinu ar masyvo field3D elementai(masyvai) sutampa su praeitais
+            //masyvais. I reiksme praeita iteracija abc
+            for (int i = abc; i >= 0; i--) {
+                if (arLygu(field3D[i], field2)) {
+                    arToksPats = true;
+                    cikliskumas = i;
                     break;
                 }
             }
-            
-            /***********Perasau massiva is paskutinio i pries pirma********/
+            /**
+             * *********Perasau massiva is paskutinio i pries pirma*******
+             */
             System.out.println("*************" + (abc + 1) + "***************");
             for (int i = 0; i < field2.length; i++) {
                 for (int j = 0; j < field2.length; j++) {
@@ -107,33 +117,55 @@ public class GameOfLife {
                 }
                 System.out.println();
             }
-            if(arToksPats) {
+            if (arToksPats) {
                 System.out.println("");
-                System.out.println("Mikroorganizmai susigyveno su " + (abc+1) + " karta");
+                System.out.println("Mikroorganizmai susigyveno su " + (abc + 1) + " karta");
+                System.out.println("Mikroorganizmu ciklas prasidejo nuo " + cikliskumas  +
+                        " kartos");
                 break;
             }
-            if(arMireVisi(field2)) {
+            if (arMireVisi(field2)) {
                 System.out.println("");
-                System.out.println("Visi mikroorganizmai mire " + (abc+1) + " kartoje");
+                System.out.println("Visi mikroorganizmai mire " + (abc + 1) + " kartoje");
                 break;
             }
         }
-       
+
     }
-    /********************Funkcija patikrinti ar visi Mire********************/
-    public static boolean arMireVisi(char[][]array2) {
+
+    /**
+     * ******************Funkcija patikrinti ar visi Mire*******************
+     */
+    public static boolean arMireVisi(char[][] array2) {
         boolean arMire = true;
-        for(int c = 0; c < array2.length; c++) {
-            for(int b = 0; b < array2.length; b++) {
-                if(array2[c][b] == 'X') {
+        for (int c = 0; c < array2.length; c++) {
+            for (int b = 0; b < array2.length; b++) {
+                if (array2[c][b] == 'X') {
                     arMire = false;
                     break;
                 }
             }
-            if(!arMire) {
+            if (!arMire) {
                 break;
             }
         }
         return arMire;
+    }
+
+    //Funkcija kuri tikrina ar esamas ir pries tai buves masyvas yra lygus
+    public static boolean arLygu(char[][] arr1, char[][] arr2) {
+        boolean same = true;
+        for (int c = 0; c < arr1.length; c++) {
+            for (int d = 0; d < arr1.length; d++) {
+                if (arr1[c][d] != arr2[c][d]) {
+                    same = false;
+                    break;
+                }
+            }
+            if (!same) {
+                break;
+            }
+        }
+        return same;
     }
 }
