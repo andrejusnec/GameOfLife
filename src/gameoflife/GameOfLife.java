7623217c31126cbc8a1c62ee8b54ef88069a9ee5
andrejusnec <73877307+andrejusnec@users.noreply.github.com>
2021-01-18 12:20:12 +0200

    2 lygis
    Tikrinu su praeita iteracija ar nesikartoja + tikrinu ar mire ir nutraukiu zaidima

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -21,6 +21,7 @@ public class GameOfLife {
         int[] iV = new int[3];
         int[] jV = new int[3];
         boolean flagA, flagB; // flagA tai a lenteles ribos, flagB stulpeliu ribos
+        boolean arToksPats, arMire; 
         for (int i = 0; i < field.length; i++) {
                 for (int j = 0; j < field.length; j++) {
                     System.out.print(field[i][j] + " ");
@@ -28,6 +29,8 @@ public class GameOfLife {
                 System.out.println();
             }
         for (int abc = 0; abc < 50; abc++) {
+            arToksPats = true;
+            arMire = true;
             System.out.println("");
             for (int a = 0; a < field.length; a++) {
                 for (int b = 0; b < field.length; b++) {
@@ -68,7 +71,7 @@ public class GameOfLife {
                             }
                         }
                     }
-                    //KAMPU TIKRINIMAS
+                    //Taisykliu taikimas
                     if (field[a][b] == '.' && count == 3) {
                         field2[a][b] = 'X';
                     } else if (field[a][b] == 'X' && (count == 3 || count == 2)) {
@@ -80,12 +83,23 @@ public class GameOfLife {
                     }
                 }
             }
-            /*if(field[i][j] == 'X'){
-    } */
             /**
-             * *************************print****************************
-             */
-            System.out.println("******************************");
+             * *************************Tikrinu ar sutampa esantis ir buves masyvai****************************/
+            for(int c = 0; c < field.length; c++) {
+                
+                for(int d = 0; d < field.length; d++) {
+                    if(field[c][d] != field2[c][d]) {
+                        arToksPats = false;
+                        break;
+                    }
+                }
+                if(!arToksPats) {
+                    break;
+                }
+            }
+            
+            /***********Perasau massiva is paskutinio i pries pirma********/
+            System.out.println("*************" + (abc + 1) + "***************");
             for (int i = 0; i < field2.length; i++) {
                 for (int j = 0; j < field2.length; j++) {
                     System.out.print(field2[i][j] + " ");
@@ -93,7 +107,33 @@ public class GameOfLife {
                 }
                 System.out.println();
             }
+            if(arToksPats) {
+                System.out.println("");
+                System.out.println("Mikroorganizmai susigyveno su " + (abc+1) + " karta");
+                break;
+            }
+            if(arMireVisi(field2)) {
+                System.out.println("");
+                System.out.println("Visi mikroorganizmai mire " + (abc+1) + " kartoje");
+                break;
+            }
         }
-
+       
+    }
+    /********************Funkcija patikrinti ar visi Mire********************/
+    public static boolean arMireVisi(char[][]array2) {
+        boolean arMire = true;
+        for(int c = 0; c < array2.length; c++) {
+            for(int b = 0; b < array2.length; b++) {
+                if(array2[c][b] == 'X') {
+                    arMire = false;
+                    break;
+                }
+            }
+            if(!arMire) {
+                break;
+            }
+        }
+        return arMire;
     }
 }
